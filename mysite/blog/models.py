c8f6430a3638e891f98d49ce3e0e0dc1fe8aab33
Siegfried <Cilob_Bisolmafol@outlook.com>
2022-06-14 19:08:28 +0700

    3-ой коммит, добавил URL, VIEW, Канонические URL'S, HTML шаблоны(список постов, страница поста), в модели поста добавил менеджера(для sql)  скачал и закинул папу с css,

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published') #метод get_queryset() менеджера возвращает Queryset
+# Queryset коллекция объектов, полученных из базы данных
+# Переопределили и добавили фильтр
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -19,6 +27,12 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager() # менеджер по умолчанию
+    published = PublishedManager() # новый менеджер
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',) # по убыванию даты "-"
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from.models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all() # запрашиваем все "published" посты из базы данных
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
+                             publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+"""Контекст
+ Если шаблон имеет вид  <p>Hello {% first_name %}.</p>
+И если вы передаете переменную first_name в контексте:
+render(request,'users/register.html', {'form': form, 'first_name': 'John'})
+Шаблон будет отображать Hello John.
+То, что вы предоставляете в контексте, доступно в шаблоне
+"""
+
+
+
+
