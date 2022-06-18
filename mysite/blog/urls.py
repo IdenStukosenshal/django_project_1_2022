70a3e9e84f1440a7d622d81f2321123a0d376b9f
Siegfried <Cilob_Bisolmafol@outlook.com>
2022-06-18 15:02:38 +0700

    6-ой коммит, добавил функцию поделиться постом с помощью e-mail, нужно ввести адрес, для которого настроен SMTP и адрес получателя

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,9 +6,9 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     #path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post123>/', views.post_detail, name='post_detail'),
-
+    path('', views.PostListView.as_view(), name='post_list'), # name='post_list' это имя URL, которое будет использовано, чтобы идентифицировать его
+    path('<int:year>/<int:month>/<int:day>/<slug:post123>/', views.post_detail, name='post_detail'), # reverse('blog:post_detail', ...
+    path('<int:post_id>/share/', views.post_share, name='post_share')
 
 
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm1
+from django.core.mail import send_mail
 
 def post_list(request):
     object_list = Post.published.all() # запрашиваем все "published" посты из базы данных
@@ -17,10 +19,13 @@ def post_list(request):
         #если номер страницы больше общего кол-ва страниц, возвращаем последнюю
         posts = paginator.page(paginator.num_pages)
 
+    return render(request, 'blog/post/list.html',
+                  {'page': page, 'posts': posts})  # Передаём номер страницы и объект в шаблон
+
     #posts = Post.published.all()
     #return render(request, 'blog/post/list.html', {'posts': posts})
 
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts}) #Передаём номер страницы и объект в шаблон
+
 
 def post_detail(request, year, month, day, post123):
     post = get_object_or_404(Post, slug=post123, status='published', publish__year=year,
@@ -40,9 +45,27 @@ render(request,'users/register.html', {'form': form, 'first_name': 'John'})
 
 class PostListView(ListView):
     """Аналог post_list"""
-    queryset = Post.published.all()
-    context_object_name = "posts"
+    queryset = Post.published.all()  #если использовать model=Post, будет менеджер по умолчанию (objects)
+    context_object_name = "posts"  # если не использовать, по умолчанию переменная контекста будет object_list
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+def post_share(request, post_id): # Получение статьи по id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST': #Форма была отправлена на сохранение, отправляется методом POST, если GET - пустая форма
+        form = EmailPostForm1(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data #словарь с полями формы и их значениями, если валидац не пройдена то в cleaned_data попадут только корректные поля
+            # Отправка электронной почты
+            post_url = request.build_absolute_uri(post.get_absolute_url()) #абсолютная ссылка на статью
+
+            subject = f'{cd["name"]} ({cd["email"]}) recommends you reading" {post.title}"'
+            message = f'Read"{post.title}" at {post_url}\n\n{cd["name"]}\'s comments: {cd["comments"]}'
+            send_mail(subject, message, '', [cd["to"],]) # https://django.fun/docs/django/ru/4.0/topics/email/
+            sent = True # Требуются параметры subject, message, from_email и recipient_list, from_email: Строка. Если None, Django будет использовать значение параметра DEFAULT_FROM_EMAIL
+            return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+    else:
+        form = EmailPostForm1()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
