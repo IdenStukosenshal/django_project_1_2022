501ac9e00592a72079dc7ed0a0b6ab7db4556923
Siegfried <Cilob_Bisolmafol@outlook.com>
2022-06-26 16:52:10 +0700

    16-ый коммит, добавлена xml карта сайта, хз зачем

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -1,12 +1,16 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Post
 
+
 class PostSitemap(Sitemap):
+    """changefreq и priority показывают частоту обновления
+страниц статей и степень их совпадения с тематикой сайта (максимальное значение 1)"""
+
     changefreq = 'weekly'
     priority = 0.9
 
     def items(self):
         return Post.published.all()
 
-    def lastmod(self, obj):
+    def lastmod(self, obj): # Метод lastmod принимает каждый объект из результата вызова items() и возвращает время последней модификации статьи
         return obj.updated
