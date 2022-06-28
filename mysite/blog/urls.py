a8b552403b2cf2a9136297f3d458def7d82c51ea
Siegfried <Cilob_Bisolmafol@outlook.com>
2022-06-28 21:03:30 +0700

    Теперь сайт работает на postgres, добавил Полнотекстовый поиск по сайту
    Добавить на будущее:
    * Форму для создания постов
    * Регистрацию пользователей

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     #path('', views.PostListView.as_view(), name='post_list'), # name='post_list' это имя URL, которое будет использовано, чтобы идентифицировать его
     path('<int:year>/<int:month>/<int:day>/<slug:post123>/', views.post_detail, name='post_detail'), # reverse('blog:post_detail', ...
     path('<int:post_id>/share/', views.post_share, name='post_share'), # получаем url из detail/html
+    path('search/', views.post_search, name='post_search'),
+
 
 
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from.models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm1, CommentForm
+from .forms import EmailPostForm1, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+
+
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all() # запрашиваем все "published" посты из базы данных
@@ -89,4 +92,17 @@ def post_share(request, post_id): # Получение статьи по id
     else:
         form = EmailPostForm1()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+# GET запрос используется чтобы получить данные, а POST чтобы отправить.
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET) # Поисковый запрос будет отправляться методом GET, чтобы результирующий URL содержал в себе фразу поиска в параметре query
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        results = Post.objects.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
